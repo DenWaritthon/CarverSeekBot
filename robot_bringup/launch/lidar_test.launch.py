f5062594d39ac0bf799bf010b7eba318ac07651f
ThepokKung <pok23250@gmail.com>
2025-05-25 15:02:34 +0700

    Update Merge lidar

--- robot_bringup/launch/lidar_test.launch.py
+++ robot_bringup/launch/lidar_test.launch.py
@@ -3,9 +3,10 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration
-from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from ament_index_python.packages import get_package_share_directory
+from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import ComposableNodeContainer, Node
+from launch_ros.descriptions import ComposableNode
+
 
 import math
 def generate_launch_description():
@@ -13,7 +14,7 @@ def generate_launch_description():
     channel_type = LaunchConfiguration('channel_type', default='serial')
     serial_port = LaunchConfiguration('serial_port', default='/dev/rplidar')
     serial_baudrate = LaunchConfiguration('serial_baudrate', default='115200')
-    rplidar_frame_id = LaunchConfiguration('frame_id', default='back_lidar')
+    rplidar_frame_id = LaunchConfiguration('frame_id', default='back_lidar_link')
     inverted = LaunchConfiguration('inverted', default='false')
     angle_compensate = LaunchConfiguration('angle_compensate', default='true')
     scan_mode = LaunchConfiguration('scan_mode', default='Boost')
@@ -83,14 +84,16 @@ def generate_launch_description():
             arguments=[
                 './src/sick_scan_xd/launch/sick_tim_7xxS.launch',
                 'hostname:=192.168.1.191',
-                'frame_id:=front_lidar',
-                'tf_base_frame_id:=base_link',
-                'tf_base_lidar_xyz_rpy:=0.3,0.0,0.0,3.14,0.0,0.0',
+                'frame_id:=front_lidar_link',
+                'tf_base_frame_id:=chassis_link',
+                'tf_base_lidar_xyz_rpy:=0.465,0.0,-0.2,3.14,0.0,0.0',
             ],
             remappings=[
                 ('/scan', '/f_scan')
             ]
         )
+    
+    #<origin xyz="0.465 0 -0.2" rpy="3.14 0 0"/>
     
     # Static transform for front LIDAR
     # front_lidar_transform = Node(
@@ -103,13 +106,56 @@ def generate_launch_description():
     # )
     
     # Static transform for back LIDAR
-    back_lidar_transform = Node(
-        package='tf2_ros',
-        executable='static_transform_publisher',
-        name='back_lidar_broadcaster',
-        arguments=['-0.3', '0.0', '0.0', '1.57', '0.0', '0.0', 'base_link', 'back_lidar'],
-        # Note: back lidar has 180 degree rotation (math.pi) to face backward
-        output='screen'
+    # back_lidar_transform = Node(
+    #     package='tf2_ros',
+    #     executable='static_transform_publisher',
+    #     name='back_lidar_broadcaster',
+    #     arguments=['-0.3', '0.0', '0.0', '1.57', '0.0', '0.0', 'base_link', 'back_lidar'],
+    #     # Note: back lidar has 180 degree rotation (math.pi) to face backward
+    #     output='screen'
+    # )
+
+    #mearge lidar
+    dual_laser_merger_node = ComposableNodeContainer(
+        name='merger_container',
+        namespace='',
+        package='rclcpp_components',
+        executable='component_container',
+        composable_node_descriptions=[
+            ComposableNode(
+                package='dual_laser_merger',
+                plugin='merger_node::MergerNode',
+                name='dual_laser_merger',
+                parameters=[
+                    {'laser_1_topic': '/f_scan'},
+                    {'laser_2_topic': '/b_scan'},
+                    {'merged_topic': '/scan'},
+                    {'target_frame': 'base_footprint'},
+                    # {'laser_1_x_offset': 0.465},
+                    # {'laser_1_y_offset': 0.0},
+                    # {'laser_1_yaw_offset': 3.14159},
+                    # {'laser_2_x_offset': -0.465},
+                    # {'laser_2_y_offset': 0.0},
+                    # {'laser_2_yaw_offset': -1.57079632679},
+                    {'tolerance': 0.01},
+                    {'queue_size': 5},
+                    {'angle_increment': 0.001},
+                    {'scan_time': 0.067},
+                    {'range_min': 0.01},
+                    {'range_max': 25.0},
+                    {'min_height': -1.0},
+                    {'max_height': 1.0},
+                    {'angle_min': -3.141592654},
+                    {'angle_max': 3.141592654},
+                    {'inf_epsilon': 1.0},
+                    {'use_inf': False},
+                    {'allowed_radius': 0.45},
+                    {'enable_shadow_filter': True},
+                    {'enable_average_filter': True},
+                ],
+            )
+        ],
+        output='screen',
     )
 
     return LaunchDescription([
@@ -122,6 +168,7 @@ def generate_launch_description():
         declare_scan_mode,
         sllidar_node,
         sick_scan_node,
+        dual_laser_merger_node
         # front_lidar_transform,
-        back_lidar_transform
+        # back_lidar_transform
     ])
